f1fc4ecda0dafc075333934ff1e4d7f55d92e918
Sally <sally@example.com>
2026-04-13 18:54:11 +0800

    fix: 恢复 Wagtail Post(Page) 模型

--- posts/models.py
+++ posts/models.py
@@ -1,58 +1,43 @@
 from django.db import models
 from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
-from wagtail.blocks import RichTextBlock, RawHTMLBlock
+from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-from wagtail.images.models import Image
-
+from wagtail.images.blocks import ImageChooserBlock   # 可选
 
 class Post(Page):
-    """Wagtail Post Page"""
+    """Wagtail 博客文章模型"""
 
-    template = "posts/post_detail.html"
-
-    publish_date = models.DateField("Publish Date")
-
+    date = models.DateField("date", auto_now_add=False)
     category = models.CharField(
         max_length=50,
         choices=[
-            ('germany', 'deutschland verstehen'),
-            ('german-learning', 'deutsch lernen'),
-            ('coding', 'coding journey'),
+            ('germany', 'Deutschland verstehen'),
+            ('german-learning', 'Deutsch lernen'),
+            ('coding', 'Coding Journey'),
         ],
-        default='coding',
-        verbose_name="Category"
+        default='german-learning',
     )
-
     featured_image = models.ForeignKey(
-        Image,
+        'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name="Featured Image"
+        related_name='+'
     )
+    body = RichTextField("body", blank=True)
 
-    body = StreamField([
-        ('rich_text', RichTextBlock(
-            features=['h2', 'h3', 'h4', 'bold', 'italic', 'link', 'code', 'image', 'hr', 'blockquote'],
-            label="Rich Text"
-        )),
-        ('raw_html', RawHTMLBlock(label="Raw HTML")),
-    ], blank=True, use_json_field=True, verbose_name="Body")
-
+    # Wagtail 后台编辑面板
     content_panels = Page.content_panels + [
         MultiFieldPanel([
-            FieldPanel('publish_date'),
+            FieldPanel('date'),
             FieldPanel('category'),
             FieldPanel('featured_image'),
-        ], heading="Post Information"),
+        ], heading="article information"),
         FieldPanel('body'),
     ]
+
+    template = "posts/post_detail.html"   # 使用你已有的模板
 
     class Meta:
         verbose_name = "Blog Post"
         verbose_name_plural = "Blog Posts"
-
-    def __str__(self):
-        return self.title
