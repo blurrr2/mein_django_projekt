1bf9ea13fc8ff62bfeae6d952299ea25b4257cc1
Sally <sally@example.com>
2026-04-13 17:18:20 +0800

    Fix deployment issues: ALLOWED_HOSTS, template fields, requirements encoding

--- meinprojekt/settings.py
+++ meinprojekt/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "meinprojekt.urls"
 
-WAGTAILADMIN_BASE_URL = 'http://127.0.0.1:8000'
+WAGTAILADMIN_BASE_URL = os.getenv('WAGTAILADMIN_BASE_URL', 'http://127.0.0.1:8000')
 
 TEMPLATES = [
     {
