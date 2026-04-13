64c0dd819e4e2737027b8a3279e59636183e7136
Sally <sally@example.com>
2026-04-13 19:22:23 +0800

    fix: views.py 改用 date 字段

--- posts/views.py
+++ posts/views.py
@@ -2,20 +2,9 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import Post
 
-def posts_list(request):
-    posts = Post.objects.live().order_by('-publish_date')
-    return render(request, 'posts/posts_list.html', {'posts': posts})
-
-def post_detail(request, slug):
-    try:
-        post = Post.objects.live().get(slug=slug)
-    except Post.DoesNotExist:
-        raise Http404("This post does not exist")
-    return render(request, 'posts/post_detail.html', {'post': post})
-
 # ==================== Category List Views ====================
 def germany_list(request):
-    posts = Post.objects.live().filter(category='germany').order_by('-publish_date')
+    posts = Post.objects.live().filter(category='germany').order_by('-date')
     return render(request, 'posts/posts_list.html', {
         'posts': posts,
         'title': 'Deutschland verstehen',
@@ -23,7 +12,7 @@ def germany_list(request):
     })
 
 def german_learning_list(request):
-    posts = Post.objects.live().filter(category='german-learning').order_by('-publish_date')
+    posts = Post.objects.live().filter(category='german-learning').order_by('-date')
     return render(request, 'posts/posts_list.html', {
         'posts': posts,
         'title': 'Deutsch lernen',
@@ -31,9 +20,16 @@ def german_learning_list(request):
     })
 
 def coding_list(request):
-    posts = Post.objects.live().filter(category='coding').order_by('-publish_date')
+    posts = Post.objects.live().filter(category='coding').order_by('-date')
     return render(request, 'posts/posts_list.html', {
         'posts': posts,
         'title': 'Coding Journey',
         'category': 'coding'
     })
+
+def post_detail(request, slug):
+    try:
+        post = Post.objects.live().get(slug=slug)
+    except Post.DoesNotExist:
+        raise Http404("This post does not exist")
+    return render(request, 'posts/post_detail.html', {'post': post})
