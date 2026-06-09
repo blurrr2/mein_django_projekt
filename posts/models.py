2b171135343437281cb230eb5140223ef0b8c0d5
Sally <sally@example.com>
2026-06-09 19:38:38 +0800

    fix: remove QuoteBlock

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,6 @@ from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.blocks import (
     RichTextBlock,
     RawHTMLBlock,
-    QuoteBlock,           # 修正为 QuoteBlock
 )
 
 from wagtail.contrib.table_block.blocks import TableBlock
@@ -16,19 +15,15 @@ from wagtail.images.blocks import ImageChooserBlock
 from wagtailmarkdown.blocks import MarkdownBlock
 from wagtail.embeds.blocks import EmbedBlock
 
-# 如果你安装了 wagtail-code-block，可以取消注释下面这行
-# from wagtailcodeblock.blocks import CodeBlock
-
 
 class Post(Page):
     """Wagtail 博客文章模型（支持多种格式）"""
 
     publish_date = models.DateField(
-    verbose_name="publish date",
-    default=timezone.now,
-)
-    
-    
+        verbose_name="publish date",
+        default=timezone.now,
+    )
+
     category = models.CharField(
         max_length=50,
         choices=[
@@ -38,7 +33,7 @@ class Post(Page):
         ],
         default='german-learning',
     )
-    
+
     featured_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -46,26 +41,24 @@ class Post(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    
+
     body = StreamField([
         ('rich_text', RichTextBlock(label="Rich text", icon="doc-full")),
         ('markdown', MarkdownBlock(label="Markdown", icon="code")),
-        #('code', CodeBlock(label="Code", icon="code")) if 'CodeBlock' in globals() else None,  # 安全处理
         ('image', ImageChooserBlock(label="Image", icon="image")),
         ('embed', EmbedBlock(label="Embed media", icon="media")),
         ('table', TableBlock(label="Table", icon="table")),
-        ('quote', QuoteBlock(label="Quote", icon="openquote")),
         ('raw_html', RawHTMLBlock(label="Raw HTML", icon="code")),
     ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
-    MultiFieldPanel([
-        FieldPanel('publish_date'),  # ← 改这里
-        FieldPanel('category'),
-        FieldPanel('featured_image'),
-    ], heading="Article Information"),
-    FieldPanel('body'),
-]
+        MultiFieldPanel([
+            FieldPanel('publish_date'),
+            FieldPanel('category'),
+            FieldPanel('featured_image'),
+        ], heading="Article Information"),
+        FieldPanel('body'),
+    ]
 
     template = "posts/post_detail.html"
 
